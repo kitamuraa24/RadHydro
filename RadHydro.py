f420a64f2b9bbc1b2ef4a49a8a8671f0dd5240be
kitamuraa24 <kitamuraa@tamu.edu>
2025-03-17 17:02:04 -0500

    Added catch for MaterialProperties toi ensure that the dim of material property arrays all agree with one another. Modified syntax for more clarity.

--- RadHydro.py
+++ RadHydro.py
@@ -17,14 +17,14 @@ class Mesh:
     """
     def __init__(self, grid_type, dh, hb, verbosity=False):
         # Keys for acceptable grids
-        acceptable_grids = ['SLAB', 'CYL', 'SPH']
+        valid_grids = ['SLAB', 'CYL', 'SPH']
         try:
-            idx = acceptable_grids.index(grid_type)
-            self.grid_type = acceptable_grids[idx]
+            idx = valid_grids.index(grid_type)
+            self.grid_type = valid_grids[idx]
         except:
             self.grid_type = None
-            acceptable_grid_str = ', '.join(acceptable_grids)
-            raise ValueError(f"Grid type {grid_type} not supported! Acceptable values are: {acceptable_grid_str}.")
+            valid_grid_str = ', '.join(valid_grids)
+            raise ValueError(f"Grid type {grid_type} not supported! Acceptable values are: {valid_grid_str}.")
         self.dh = dh
         self.hb = hb
         self.verbose = verbosity
@@ -53,10 +53,14 @@ class Mesh:
                 self.V = cell_w
             elif self.grid_type == 'CYL':
                 self.A = 2 * np.pi * self.vertices
+                self.A = np.insert(self.A, 0, 0)
                 self.V = np.pi * (self.vertices[1:]**2 - self.vertices[:-1]**2)
+                self.V = np.insert(self.V, 0, 0)
             else: # self.grid_type == 'SPH'
                 self.A = 4 * np.pi * self.vertices**2
+                self.A = np.insert(self.A, 0, 0)
                 self.V = 4/3 * np.pi * (self.vertices[1:]**3 - self.vertices[:-1]**3)
+                self.V = np.insert(self.V, 0, 0)
             if self.verbose:
                 header = f"{'Region':<10}{'Start':<10}{'End':<10}{'Cell Count':<12}"
                 print(header)
@@ -79,6 +83,12 @@ class MaterialProperties:
             - verbosity (bool): verbosity of the process (Default: False)
     """
     def __init__(self, hb, centroids, ncells, Cv_list, Ka_list, Ks_list, verbosity=False):
+        if not (len(Cv_list) == len(Ka_list) == len(Ks_list) == len(hb)):
+            raise ValueError(
+                f"Material property lists must have length {len(hb)} (len(hb)) , "
+                f"but got lengths: Cv_list={len(Cv_list)}, Ka_list={len(Ka_list)}, Ks_list={len(Ks_list)}"
+            )
+        
         self.hb = hb
         self.centroids = centroids
         self.ncells = ncells
@@ -105,7 +115,10 @@ class MaterialProperties:
             for material, count in zip(unique_materials, counts):
                 print(f"{material:<20}{count:<20}")
             print('\n')
-    
+
+
+
+
 
 
 if __name__ == "__main__":
